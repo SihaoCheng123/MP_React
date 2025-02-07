e26ddc7cb787fc4192eff1d4aee746089e4a22ac
SihaoCheng123 <sihao.cheng@estudiante.epsum.school>
2025-02-07 12:27:29 +0100

    Directorios creados

---
+++ app/presentation/theme/AppTheme.tsx
@@ -0,0 +1,15 @@
+export const AppColors = {
+    primary: "#CCD87F",
+    secondary:"#A8A1D4",
+    tertiary: "#F8F7ED",
+    background:"#F8F7ED",
+    white: "#FFFFFF",
+    dark: "#2A2A2A",
+    black: "#000000",
+    grey: "#505050",
+    textOnPrimary: "#F5F5F5",
+    cardCategoryGrey: "#C5C6CC",
+    cardsGreyBackgroundRegisterAndRecipes : "#E7E7E7",
+    cardsIngredientsBackground : "#FBFBFB",
+    userProfileBackground : "#D8E0A6"
+}
