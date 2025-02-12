6c50776995372dec46d958071400a70ae443f1f5
F7Epsum <Federico.carassale@estudiante.epsum.school>
2025-02-12 09:14:07 +0100

    Background Screen cart added

---
+++ app/presentation/views/Cart/StyleCartScreen.tsx
@@ -0,0 +1,12 @@
+import {StyleSheet} from "react-native";
+import {AppColors} from "../../theme/AppTheme";
+
+const styleCartScreen = StyleSheet.create({
+    mainContainer:{
+        backgroundColor: AppColors.userProfileBackground,
+        width: '100%',
+        height: '100%',
+    },
+})
+
+export default styleCartScreen;
