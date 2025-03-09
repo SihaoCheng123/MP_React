a7a4fe1920f1730c71067ddbb0ee50dfa5b2cd0b
SihaoCheng123 <sihao.cheng@estudiante.epsum.school>
2025-03-09 17:16:12 +0100

    conflictos

--- app/data/sources/remote/api/ApiDelivery.tsx
+++ app/data/sources/remote/api/ApiDelivery.tsx
@@ -1,13 +1,9 @@
 import axios from 'axios';
-import {detailedRecipeInterface} from "../../../../presentation/interfaces/recipeInterface";
 
 const ApiDelivery = axios.create({
 
-<<<<<<< HEAD
-    baseURL: 'http://192.168.0.32:8080/api',
-=======
+
     baseURL: 'http://192.168.1.186:8080/api',
->>>>>>> master
     headers:{
         "Content-Type": "application/json",
     }
